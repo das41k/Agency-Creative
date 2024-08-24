2bfea066a33746a7383f1b20d0f0a08b1cd8b84d
das41k <armenpetrosan319@gmail.com>
2024-08-25 00:19:32 +0300

    fixed problems and develop about us

--- src/js/componets/burger.js
+++ src/js/componets/burger.js
@@ -1,19 +1,33 @@
 export const useBurger = () => {
-    document.addEventListener('click',burgerInit);
-    function burgerInit(e) {
-        const burgerIcon = e.target.closest('.header-burger');
-        const burgerLink = e.target.closest('.header__item-link');
-        if (!burgerIcon && !burgerLink) return;
-        
-        const burgerMenuList = document.querySelector('.header__list');
-        const burgerMenuOp = document.querySelector('.header__right'); 
-        const overlay = document.querySelector('.overlay')
-        const logo = document.querySelector('.header__logo')
-        
+    const burgerIcon = document.querySelector('.header-burger');
+    const overlay = document.querySelector('.overlay');
+    const burgerLink = document.querySelector('.header__item-link')
+
+    const burgerMenuList = document.querySelector('.header__list');
+    const burgerMenuOp = document.querySelector('.header__right'); 
+    const logo = document.querySelector('.header__logo');
+    
+    function closeBurgerMenu() {
+        burgerMenuList.classList.remove('header__list--visible');
+        burgerMenuOp.classList.remove('header__right--visible')
+        overlay.classList.remove('overlay--visible');
+        burgerIcon.classList.remove('header-burger--active');
+        logo.classList.remove('header__logo--visible');
+    }
+    function toggleBurgerMenu() {
         burgerMenuList.classList.toggle('header__list--visible');
         burgerMenuOp.classList.toggle('header__right--visible')
         overlay.classList.toggle('overlay--visible');
         burgerIcon.classList.toggle('header-burger--active');
-        logo.classList.toggle('header__logo--visible');
+        logo.classList.toggle('header__logo--visible')
     }
+    document.addEventListener('click', event => {
+        const target = event.target;
+        if (target && target.classList.contains('header-burger')) {
+            toggleBurgerMenu();
+        }
+        else if (target && (target.classList.contains('header__item-link') || target.classList.contains('overlay'))) {
+            closeBurgerMenu();
+        }
+    })
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ import '/scss/main.scss';
 import { useTheme } from './componets/theme';
 import { useHeaderTabs } from './componets/headerTabs';
 import { useBurger } from './componets/burger';
+import FsLightbox from 'fslightbox';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lightbox = new FsLightbox();
+});
 useTheme();
 useHeaderTabs();
 useBurger();
