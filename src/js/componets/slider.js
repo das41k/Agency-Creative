7fd0ef63474c6ee4c167c675c3732888b1dbabbc
das41k <armenpetrosan319@gmail.com>
2024-08-27 18:15:17 +0300

    done testimonials + collaboration

---
+++ src/js/componets/slider.js
@@ -0,0 +1,30 @@
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination,Autoplay } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+
+export const useSlider = () => {
+    new Swiper('.testimonials__slider', {
+        modules: [Navigation,Pagination,Autoplay],
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        autoplay: {
+            delay: 3000,
+        },
+        navigation: {
+            nextEl: '.testimonials__slider-next',
+            prevEl: '.testimonials__slider-prev',
+        },
+        pagination: {
+            el: '.testimonials__pagination',
+            type: 'bullets',
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            }
+        }
+    })
+}
