b1c15c7194f22923cb0afbc843c90a830ffd686b
das41k <armenpetrosan319@gmail.com>
2024-08-28 16:34:15 +0300

    fixed promlems slider

--- src/js/componets/slider.js
+++ src/js/componets/slider.js
@@ -19,6 +19,7 @@ export const useSlider = () => {
         pagination: {
             el: '.testimonials__pagination',
             type: 'bullets',
+            clickable: true,
         },
         breakpoints: {
             992: {
