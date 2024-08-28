a2802a18785a8efa26e9f0051f9f962dc08c13a3
das41k <armenpetrosan319@gmail.com>
2024-08-28 16:16:09 +0300

    fixed problems pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   root: "./src",
-  base: '/AgencyCreavite/',
+  base: '/Agency-Creative/',
   server: {
     port: 3000,
   },
